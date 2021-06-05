6c5c9fba0f3a73094f204167dd590cc93e2edd55
Hari-lang <dinehari09@gmail.com>
2021-06-05 23:24:42 +0530

    Automation Practice

---
+++ src/googlee/Facebook.java
@@ -0,0 +1,15 @@
+package googlee;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook {
+	
+	System.setProperty("webdriver.chrome.driver", "C:\\Boobalahariprasath\\software\\chromedriver.exe");
+	
+	 Webdriver = new ChromeDriver();
+	
+	 driver.get("http://google.co.in");
+	 
+	 driver.findElement(By.name("q")).sendKeys("Google");
+
+}
